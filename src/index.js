ef1dda56cb641a3dc20fcce4863410eb779d7c5a
mycontourdev <98138824+mycontourdev@users.noreply.github.com>
2022-02-11 14:56:22 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 function App() {
+  const fetch = require('node-fetch');
+  fetch('https://google.com')
+    .then(res => res.text())
+    .then(text => console.log(text));
   return <h1>..</h1>
 }
 
